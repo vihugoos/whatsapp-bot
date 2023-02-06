3edbb973c728da6b33b43487a3331114b88e0782
Victor Hugo <victorhugoos@live.com>
2023-02-06 20:00:20 -0300

    feat: sendMessageToDiscordChannel function created

--- src/webhooks/discord.js
+++ src/webhooks/discord.js
@@ -1,20 +1,29 @@
 require("dotenv").config();
 const axios = require("axios");
 
-async function sendMessageToDiscordChannel({ from, type, protocol }) {
+module.exports = async function sendMessageToDiscordChannel({
+    client_name,
+    chosen_service,
+    protocol,
+    discord_user_id,
+}) {
+    const discord_core_role = "1067573586363162815";
+    let message;
+
+    if (discord_user_id) {
+        message = `Solicitação atribuída para <@!${discord_user_id}>`;
+    } else {
+        message = `**AVISO:** Esta solicitação não foi atribuída a ninguém! <@&${discord_core_role}>`;
+    }
+
     await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+        content: message,
         embeds: [
             {
-                title: `Nova solicitação de ${from} (${type})`,
-                description: `Assigned to <@!655259186111709217>, protocol: ${protocol}`,
+                title: `Nova solicitação de ${client_name}`,
+                description: `Serviço escolhido: **${chosen_service}**\nProtocolo de atendimento: **${protocol}**`,
                 color: 4437377,
             },
         ],
     });
-}
-
-sendMessageToDiscordChannel({
-    from: "Claudio",
-    type: "Agendamentos",
-    protocol: "eeaee3e8-e3cd-4242-ab08-e9d744680850",
-});
+};
